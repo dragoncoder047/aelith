875f35e14a3ce7212db76677030c73ccb2ac414a
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2024-10-01 08:26:46 -0400

    try to fix not being able to jump on a box

--- components/correctPhysics.ts
+++ components/correctPhysics.ts
@@ -1,6 +1,8 @@
 import { BodyComp, Comp, GameObj, Tag } from "kaplay";
+import { K } from "../init";
 
 export interface CorrectPhysicsComp extends Comp {
+    require: string[] // why is this necessary?
 }
 
 /**
@@ -11,6 +13,9 @@ export function correctPhysics(): CorrectPhysicsComp {
         require: ["body"],
         add(this: GameObj<BodyComp>) {
             this.onPhysicsResolve(coll => {
+                const relativeVel = this.vel.sub(coll.target.vel).project(coll.normal).rotate(coll.normal.normal().angle());
+                // TODO: this makes the player unable to jump if they are standing on a box.
+                // Need to fix this.
                 this.vel = this.vel.reject(coll.normal);
             });
         },
