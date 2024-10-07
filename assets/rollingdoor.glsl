a66ed50240abfcebb8e4574cf13719992a894301
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2024-10-07 19:59:30 -0400

    typo

--- assets/rollingdoor.glsl
+++ assets/rollingdoor.glsl
@@ -1,7 +1,7 @@
 uniform float u_moveby;
 
 vec4 frag(vec2 pos, vec2 uv, vec4 color, sampler2D tex) {
-    if (uv.x <= u_moveby) return vec4(0, 0, 0, 0);
+    if (uv.y <= u_moveby) return vec4(0, 0, 0, 0);
     uv -= vec2(0, u_moveby);
     return texture2D(tex, uv);
 }
