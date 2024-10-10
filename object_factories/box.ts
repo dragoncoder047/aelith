751fdb1b2ea5f625088585636e811fce4455c745
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2024-10-10 17:05:46 -0400

    add ability to push boxes back

--- object_factories/box.ts
+++ object_factories/box.ts
@@ -4,9 +4,9 @@ import { grabbable } from "../components/grabbable";
 import { thudder } from "../components/thudder";
 import { FRICTION, RESTITUTION, TERMINAL_VELOCITY, TILE_SIZE } from "../constants";
 import { K } from "../init";
-
-import { machine } from "./machine";
 import { player } from "../player";
+import { getMotionVector } from "../player/controls/impl";
+import { machine } from "./machine";
 
 /**
  * Components for a moveable, grabbable box.
@@ -31,9 +31,10 @@ export function box(): CompList<any> {
         K.platformEffector({
             shouldCollide(obj, normal) {
                 if (obj !== player) return true;
+                if (K.UP.eq(normal)) return false;
+                if (getMotionVector().slen() > 1.3) return true;
                 if (K.LEFT.eq(normal)) return false;
                 if (K.RIGHT.eq(normal)) return false;
-                if (K.UP.eq(normal)) return false;
                 return true;
             },
         })
