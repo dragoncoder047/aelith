f05c279144e31ad2d25007544bb3351e75373eaf
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2024-10-12 18:56:18 -0400

    tweak strings

--- startup.ts
+++ startup.ts
@@ -59,9 +59,9 @@ const chunks: TextChunk[] = [
     },
     ...command("sudo ai-assistant &", "(1) 4242 ai-assistant\nAssistant is running", "~", 1, 1),
     ...command("ai \"find the answer\"", "Segmentation fault (core dumped)"
-        + "\n[stderr](1)  + 4242 exit 139   sudo ai-assistant[/stderr]", "~", 1, 2),
+        + "\n(1)  + 4242 exit 139   ai-assistant\n[stderr]ai: error: EHOSTDOWN[/stderr]", "~", 1, 2),
     ...command("cd /sys/ai", "", "~", 3, 0.25),
-    ...command("gdb pm", "Starting debugger...", "/sys/ai", 0.25, 1),
+    ...command("gdb pm", "Starting debugger...", "/sys/ai", 0.25, 0.25),
 ];
 
 export async function doStartup() {
