8c9f9b90c4b06d9196be8b2c740a35a7dc8783b7
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2024-10-12 13:55:57 -0400

    add more continuation shtuff

---
+++ assets/recolor-red.glsl
@@ -0,0 +1,7 @@
+uniform vec4 u_targetcolor;
+
+vec4 frag(vec2 pos, vec2 uv, vec4 color, sampler2D tex) {
+    vec4 o_color = texture2D(tex, uv);
+    if (o_color.r > 0 && o_color.g == 0 && o_color.b == 0) return vec4(u_targetcolor.rgb, o_color.a);
+    return o_color;
+}
