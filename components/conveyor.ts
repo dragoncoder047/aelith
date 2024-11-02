1edc2d580279ca1a3bc89e559c9bf315b8d87d52
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2024-11-02 16:47:13 -0400

    add lots of fun challenges

--- components/conveyor.ts
+++ components/conveyor.ts
@@ -5,6 +5,7 @@ type SurfaceEffectorComp = ReturnType<typeof K.surfaceEffector>; // why is this 
 
 export interface ConveyorComp extends Comp {
     turnSpeed: number,
+    direction: 1 | -1,
     __lastX: number,
 }
 
@@ -13,23 +14,18 @@ export function conveyor(states: [string, string] = ["off", "on"], speed: number
         id: "conveyor",
         require: ["linked", "sprite", "state", "body", "surfaceEffector"],
         turnSpeed: speed,
+        direction: 1,
         __lastX: 0,
         update(this: GameObj<ConveyorComp | StateComp | SurfaceEffectorComp | SpriteComp>) {
             const x = states.indexOf(this.state);
-            this.speed = this.turnSpeed * x;
+            this.speed = this.turnSpeed * x * this.direction;
             if (this.__lastX === x) return;
-            switch (x) {
-                case 0:
-                    this.animSpeed = 0;
-                    break;
-                case 1:
-                    this.animSpeed = 1;
-                    this.play("forward");
-                    break;
-                case -1:
-                    this.animSpeed = 1;
-                    this.play("backward");
-                    break;
+            if (x === 0) {
+                this.animSpeed = 0;
+            }
+            else {
+                this.animSpeed = 1;
+                this.play(this.direction === 1 ? "forward" : "backward");
             }
             this.__lastX = x;
         },
