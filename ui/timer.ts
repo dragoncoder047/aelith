5251229729b0a3ddb0d163c60b8bed0aa6f6376f
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2024-11-08 22:09:50 -0500

    speedrun timer

---
+++ ui/timer.ts
@@ -0,0 +1,27 @@
+import { GameObj, PosComp } from "kaplay";
+import { UI } from ".";
+import { MARGIN, SCALE, TILE_SIZE } from "../constants";
+import { K } from "../init";
+import { player } from "../player";
+
+const timer = UI.add([
+    K.text("", { size: 24 / SCALE, width: TILE_SIZE * 8, align: "right" }),
+    K.anchor("topright"),
+    K.pos(),
+    {
+        update(this: GameObj<PosComp>) {
+            this.pos = K.vec2(K.width() - MARGIN, MARGIN);
+        }
+    }
+]);
+
+timer.hidden = true;
+
+var timerValue = 0;
+K.onUpdate(() => {
+    if (!player.hidden) {
+        timerValue += K.dt();
+        timer.hidden = false;
+    }
+    timer.text = `${Math.floor(timerValue / 60)}:${(timerValue % 60).toFixed(4)}`.replace(/:(\d)\./, ":0$1.");
+});
