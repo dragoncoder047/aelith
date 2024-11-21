53b3f1c67bdd88e69c6bf60aa0a549013b487c96
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2024-11-21 15:59:59 -0500

    revise conveyor

--- components/conveyor.ts
+++ components/conveyor.ts
@@ -1,32 +1,28 @@
-import { Comp, GameObj, SpriteComp /*, SurfaceEffectorComp */, StateComp } from "kaplay";
-import { K } from "../init";
-type SurfaceEffectorComp = ReturnType<typeof K.surfaceEffector>; // why is this necessary??
+import { Comp, GameObj, SpriteComp, SurfaceEffectorComp, StateComp } from "kaplay";
 
 export interface ConveyorComp extends Comp {
     turnSpeed: number,
-    direction: 1 | -1,
-    __lastX: number,
+    direction: number,
 }
 
 export function conveyor(states: [string, string] = ["off", "on"], speed: number): ConveyorComp {
+    var __lastState: string;
     return {
         id: "conveyor",
         require: ["linked", "sprite", "state", "body", "surfaceEffector"],
         turnSpeed: speed,
         direction: 1,
-        __lastX: 0,
         update(this: GameObj<ConveyorComp | StateComp | SurfaceEffectorComp | SpriteComp>) {
-            const x = states.indexOf(this.state);
-            this.speed = this.turnSpeed * x * this.direction;
-            if (this.__lastX === x) return;
-            if (x === 0) {
+            this.speed = this.turnSpeed * states.indexOf(this.state) * this.direction;
+            if (__lastState === this.state) return;
+            if (this.state === states[0]) {
                 this.animSpeed = 0;
             }
             else {
-                this.animSpeed = 1;
+                this.animSpeed = Math.abs(this.direction);
                 this.play(this.direction === 1 ? "forward" : "backward");
             }
-            this.__lastX = x;
+            __lastState = this.state;
         },
         inspect() {
             return `conveyor speed ${this.turnSpeed}`;
