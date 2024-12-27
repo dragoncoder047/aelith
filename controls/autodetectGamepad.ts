e3620ecb6683fec752837bef3449640cb7d92702
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2024-12-26 19:48:57 -0500

    tweak auto detect gamepad functions

--- controls/autodetectGamepad.ts
+++ controls/autodetectGamepad.ts
@@ -7,26 +7,37 @@ export function detectGamepadType(id: string) {
         || id.includes("dualshock")
         || (vendor === 0x054c && product === 0x09cc)) // Sony, DS4
         return "ps4";
+
     if (id.includes("ps5")
         || id.includes("dualsense")
         || (vendor === 0x054c && product === 0x0ce6)) // Sony, DS5
         return "ps5";
+
     if (id.includes("xbox") || vendor === 0x045e) // Microsoft
         return "xbox";
+
     if (id.includes("switch")
         || id.includes("joy-con")
-        || id.includes("pro controller")
         || vendor === 0x057e) // Nintendo
         return "switch";
+
     return undefined;
 }
 
-function extractVendorProductId(id: string): { vendor: number | undefined, product: number | undefined } {
-    const vendorMatch = id.match(/vendor:\s*([0-9a-f]{4})/);
-    const productMatch = id.match(/product:\s*([0-9a-f]{4})/);
+export function isSingleJoyCon(id: string) {
+    id = id.toLowerCase();
+    if (detectGamepadType(id) !== "switch") return undefined;
+    if (id.includes("joy-con") && !id.includes("l+r")) return true;
+    const { product } = extractVendorProductId(id);
+    return product === 0x2006 || product === 0x2007;
+}
 
-    const vendor = vendorMatch ? parseInt("" + vendorMatch[1], 16) : undefined;
-    const product = productMatch ? parseInt("" + productMatch[1], 16) : undefined;
+function extractVendorProductId(id: string): { vendor: number | null, product: number | null } {
+    const vendorMatch = /vendor[^\da-f]*([\da-f]{4})/i.exec(id);
+    const productMatch = /product[^\da-f]*([\da-f]{4})/i.exec(id);
+
+    const vendor = vendorMatch && parseInt("" + vendorMatch[1], 16);
+    const product = productMatch && parseInt("" + productMatch[1], 16);
 
     return { vendor, product };
 }
