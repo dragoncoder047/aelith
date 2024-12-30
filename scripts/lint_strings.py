35a5d1cc070e795648b31a813399388d842bd1a0
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2024-12-30 10:14:31 -0500

    reorganize scripts to all python

---
+++ scripts/lint_strings.py
@@ -0,0 +1,43 @@
+import json
+
+LANGS = ["en", "es", "de", "ja"]
+
+
+def get_json(path: str):
+    with open(path) as f:
+        return json.load(f)
+
+
+LANG_ENTRIES = {}
+
+for lang in LANGS:
+    LANG_ENTRIES[lang] = get_json(f"./assets/translations/{lang}.json")
+
+PATHS_ALL = set()
+PATHS_BY_LANG = {}
+
+
+def recursive_all(vars, byLang: set[str]):
+    def recur(curPath, obj):
+        if isinstance(obj, str):
+            PATHS_ALL.add(".".join(curPath))
+            byLang.add(".".join(curPath))
+        elif isinstance(obj, dict):
+            for next in obj:
+                recur(curPath + [next], obj[next])
+    recur([], vars)
+
+
+for lang in LANGS:
+    PATHS_BY_LANG[lang] = set()
+    recursive_all(LANG_ENTRIES[lang], PATHS_BY_LANG[lang])
+
+for lang in LANGS:
+    print(f"{lang}:")
+    good = True
+    for path in sorted(PATHS_ALL, reverse=True):
+        if path not in PATHS_BY_LANG[lang]:
+            print(f"  Missing translation {path}")
+            good = False
+    if good:
+        print("  All good")
