38ae9faa697d3ebb5f7c8b63e095977bb547ff92
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2025-01-01 23:15:31 -0500

    optimize for transparent

--- assets/translate.glsl
+++ assets/translate.glsl
@@ -7,6 +7,6 @@ vec4 frag(vec2 pos, vec2 uv, vec4 color, sampler2D tex) {
     vec2 quad_botright = u_quad_topleft + u_quad_wh;
     if (uv_p.x < u_quad_topleft.x || uv_p.x > quad_botright.x
             || uv_p.y < u_quad_topleft.y || uv_p.y > quad_botright.y)
-        return vec4(0, 0, 0, 0);
+        discard;
     return texture2D(tex, uv_p);
 }
