1b604c47e768f9bb6f52e25c5d027399e023c911
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2025-01-01 22:29:35 -0500

    broken ladder don't need no area()

--- object_factories/brokenLadder.ts
+++ object_factories/brokenLadder.ts
@@ -1,4 +1,4 @@
-import { Tag } from "kaplay";
+import { GameObj, Tag } from "kaplay";
 import { randomFrame } from "../components/randomFrame";
 import { K } from "../init";
 import { defaults } from "./default";
@@ -6,10 +6,15 @@ import { defaults } from "./default";
 export function brokenLadder() {
     return [
         K.sprite("broken_ladder"),
-        ...defaults({ collisionIgnore: ["*"] }),
+        ...defaults(),
         K.offscreen({ hide: true }),
         randomFrame(),
         "ladder" as Tag,
         "raycastIgnore" as Tag,
+        {
+            add(this: GameObj) {
+                this.unuse("area");
+            }
+        }
     ];
 }
