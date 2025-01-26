36ac3da5863ce8ac170cf2940c7968905f1e47e0
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2025-01-26 12:59:37 -0500

    fix recapture

--- components/continuationCore.ts
+++ components/continuationCore.ts
@@ -82,6 +82,14 @@ export function continuationCore(
             const delta = this.captured.playerPos.sub(p);
             const reverseDelta = K.vec2(0);
 
+            if (this.params.recapture) {
+                // Capture a continuation from right here so the player can go back.
+                const temp = (this.trappedBy as GameObj<ContinuationTrapComp>).params;
+                this.trappedBy.params = this.params;
+                this.trappedBy.capture();
+                this.trappedBy.params = temp;
+            }
+
             if (this.params.reverseTeleport) {
                 // do move
                 reverseDelta.x = -delta.x;
@@ -137,13 +145,6 @@ export function continuationCore(
                     obj.ignoreTriggerTimeout = 5;
             }
             if (this.params.oneshot) this.destroy();
-            if (this.params.recapture) {
-                // Capture a continuation from right here so the player can go back.
-                const temp = (this.trappedBy as GameObj<ContinuationTrapComp>).params;
-                this.trappedBy.params = this.params;
-                this.trappedBy.capture();
-                this.trappedBy.params = temp;
-            }
         },
         draw(this: GameObj<PosComp | ContinuationComp | RotateComp>) {
             if (this.params.reverseTeleport) return;
