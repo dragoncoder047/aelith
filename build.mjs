e76e1ac90f2d58f661030331a5699ac761a904b3
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2025-02-05 10:31:24 -0500

    compress strings, JSON, text, and shaders
    for some reason, this makes it load *faster*, weird

--- build.mjs
+++ build.mjs
@@ -2,12 +2,133 @@ import * as esbuild from "esbuild";
 import * as fs from "node:fs";
 import packageJSON from "./package.json" with { type: "json" };
 
+// sanity check
+if (decompress(compress("a".repeat(100))) !== "a".repeat(100)) throw "bad";
+
+const compressionRuntimeSrc = `/* Auto-generated file... */
+export ${decompress}
+`;
+
+/**
+ * @param {string} path
+ * @returns {Promise<boolean>}
+ */
+function exists(path) {
+    return new Promise(res => fs.stat(path, fs.constants.F_OK, err => res(!err)));
+}
+
+/**
+ * @param {string} contents
+ * @param {string} path
+ * @returns {string}
+ */
+function RawTextCompressedModule(contents, path) {
+    const m = `/* Auto-generated file... */
+import { decompress } from "$$DECOMPRESSOR_RUNTIME";
+const ${nameify(path)}_crunch = ${JSON.stringify(compress(contents))}
+const ${nameify(path)} = decompress(${nameify(path)}_crunch);
+export default ${nameify(path)};
+`;
+    return m;
+}
+
+/**
+ * @param {string} json
+ * @param {string} path
+ * @returns {string}
+ */
+function JSONCompressedModule(json, path) {
+    const m = `/* Auto-generated file... */
+import { decompress } from "$$DECOMPRESSOR_RUNTIME";
+const ${nameify(path)}_crunch = ${JSON.stringify(compress(json))};
+const ${nameify(path)} = JSON.parse(decompress(${nameify(path)}_crunch));
+export default ${nameify(path)};
+`;
+    return m;
+}
+
+/**
+ * @param {string} string
+ * @returns {string}
+ */
+function nameify(string) {
+    const suffix = string.substring(string.lastIndexOf("/"));
+    return suffix.replace(/[^\w]/g, "_").replace(/^_+/, "");
+}
+
+// compression and decompression algorithms taken from https://github.com/psyked/LZW-js
+
+/**
+ * @param {string} contents
+ * @returns {string}
+ */
+function compress(string) {
+    var dict = {};
+    var data = encodeURIComponent(string).split("");
+    var out = [];
+    var ch;
+    var b = data[0];
+    var c = 256;
+    var i, l;
+    for (i = 1, l = data.length; i < l; i++) {
+        ch = data[i];
+        if (dict[b + ch] != null) {
+            b += ch;
+        }
+        else {
+            out.push(b.length > 1 ? dict[b] : b.charCodeAt(0));
+            dict[b + ch] = c;
+            c++;
+            b = ch;
+        }
+    }
+    out.push(b.length > 1 ? dict[b] : b.charCodeAt(0));
+    for (i = 0, l = out.length; i < l; i++) {
+        out[i] = String.fromCharCode(out[i]);
+    }
+    return out.join("");
+}
+
+/**
+ * @param {string} compressed
+ * @returns {string}
+ */
+function decompress(string) {
+    var dict = {};
+    var data = (string + "").split("");
+    var ch = data[0];
+    var w = ch;
+    var out = [ch];
+    var c = 256;
+    var b;
+    for (var i = 1; i < data.length; i++) {
+        var currCode = data[i].charCodeAt(0);
+        if (currCode < 256) {
+            b = data[i];
+        }
+        else {
+            b = dict[currCode] ? dict[currCode] : (w + ch);
+        }
+        out.push(b);
+        ch = b.charAt(0);
+        dict[c] = w + ch;
+        c++;
+        w = b;
+    }
+    return decodeURIComponent(out.join(""));
+}
+
+// ---------------------------------------------------------------------------
+// MARK: config and CLI
+
+
 /** @type {esbuild.BuildOptions} */
 const config = {
     bundle: true,
     sourcemap: true,
     minify: true,
     platform: "browser",
+    charset: "utf8",
     loader: {
         ".png": "dataurl",
         ".glsl": "text",
@@ -19,40 +140,59 @@ const config = {
     target: "esnext",
     treeShaking: true,
     outfile: "build/debugger.js",
+    plugins: [
+        {
+            name: "nonexistent_go_bye_bye",
+            setup(build) {
+                build.onResolve({ filter: /\.p$/ }, async args => {
+                    if (!(await exists(args.path))) return { external: true };
+                })
+            },
+        },
+        {
+            name: "compress_text",
+            setup(build) {
+                build.onResolve({ filter: /^\$\$DECOMPRESSOR_RUNTIME$/ }, () => {
+                    return {
+                        path: "$$DECOMPRESSOR_RUNTIME",
+                        namespace: "compress_text",
+                    }
+                });
+                build.onLoad({ filter: /\$\$DECOMPRESSOR_RUNTIME/, namespace: "compress_text" }, () => {
+                    return { contents: compressionRuntimeSrc };
+                })
+                build.onLoad({ filter: /\.txt$/ }, args => {
+                    return {
+                        contents: RawTextCompressedModule(fs.readFileSync(args.path), args.path)
+                    }
+                });
+                build.onLoad({ filter: /\.glsl$/ }, args => {
+                    return {
+                        contents: RawTextCompressedModule(fs.readFileSync(args.path), args.path)
+                    }
+                });
+                build.onLoad({ filter: /\.json$/ }, args => {
+                    return {
+                        contents: JSONCompressedModule(fs.readFileSync(args.path), args.path)
+                    }
+                });
+            }
+        }
+    ]
 };
 
-var watch = false;
-if (process.argv.includes("-w")) watch = true;
-
-if (watch) {
-    const ctx = await esbuild.context({
-        ...config,
-        plugins: [
-            {
-                name: "logger",
-                setup(build) {
-                    build.onEnd(result => {
-                        if (result.errors.length == 0)
-                            console.error(`[${new Date().toISOString()}] rebuilt ${config.outfile} success!`);
-                        else
-                            console.error(`[${new Date().toISOString()}] failed to build ${config.outfile}!`)
-                    });
-                },
-            },
-            {
-                name: "nonexistent_go_bye_bye",
-                setup(build) {
-                    build.onResolve({ filter: /\.p$/ }, async args => {
-                        if (!(await exists(args.path))) return { external: true };
-                    })
-                },
-            }
-        ],
+if (process.argv.includes("-w")) {
+    config.plugins.push({
+        name: "logger",
+        setup(build) {
+            build.onEnd(result => {
+                if (result.errors.length == 0)
+                    console.error(`[${new Date().toISOString()}] rebuilt ${config.outfile} success!`);
+                else
+                    console.error(`[${new Date().toISOString()}] failed to build ${config.outfile}!`)
+            });
+        },
     });
-    await ctx.watch();
+    await esbuild.context(config).then(ctx => ctx.watch());
 }
 else await esbuild.build(config);
-
-function exists(path) {
-    return new Promise(res => fs.stat(path, fs.constants.F_OK, err => res(!err)));
-}
