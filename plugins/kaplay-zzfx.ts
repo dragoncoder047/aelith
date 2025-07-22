fcacc1d526aad67cd1be279a8d6afcc07fa25f5a
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2025-07-22 13:26:55 -0400

    revert zzfx caching as that wasn't the problem

--- plugins/kaplay-zzfx.ts
+++ plugins/kaplay-zzfx.ts
@@ -12,20 +12,14 @@ export interface ZzFXPlugin {
 export function kaplayZzFX(K: KAPLAYCtx): ZzFXPlugin {
     const oldPlay = K.play;
     const zzfxMap = new Map<string, (number | undefined)[]>();
-    const cache = new Map<string, AudioBuffer>();
     // use the same audio context if the user calls zzfx() directly
     ZZFX.x = K.audioCtx;
     return {
         play(src, options) {
             if (typeof src === "string" && zzfxMap.has(src)) {
-                var buffer = cache.get(src);
-                // Cache the samples to prevent needing to regenerate the samples every time
-                if (!buffer) {
-                    const samples = ZZFX.buildSamples(...(zzfxMap.get(src)!.with(1, 0)));
-                    buffer = K.audioCtx.createBuffer(1, samples.length, ZZFX.sampleRate);
-                    buffer.getChannelData(0).set(samples, 0);
-                    cache.set(src, buffer);
-                }
+                const samples = ZZFX.buildSamples(...(zzfxMap.get(src)!.with(1, 0)));
+                const buffer = K.audioCtx.createBuffer(1, samples.length, ZZFX.sampleRate);
+                buffer.getChannelData(0).set(samples, 0);
                 return oldPlay(new K.SoundData(buffer), { ...options, detune: (options?.detune ?? 0) * (1 + (Math.random() - 0.5) * (zzfxMap.get(src)![1] ?? 0.05)) });
             } else return oldPlay(src, options);
         },
