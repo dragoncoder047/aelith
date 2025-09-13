f39c2cb061d787805b413bda5d0a8a2d2c9df356
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2025-09-13 13:41:19 -0400

    premultiplied alpha was screwing with my shaders

--- src/assets/shaders/invert.glsl
+++ src/assets/shaders/invert.glsl
@@ -1,7 +1,7 @@
 uniform vec3 u_bg_color;
 
 vec4 frag(vec2 pos, vec2 uv, vec4 color, sampler2D tex) {
-    vec4 pixel = def_frag();
+    vec4 pixel = texture2D(tex, uv);
     if (pixel.a > 0.0) discard;
     return vec4(u_bg_color / 255., 1.);
 }
--- src/assets/shaders/recolorRed.glsl
+++ src/assets/shaders/recolorRed.glsl
@@ -1,8 +1,8 @@
 uniform vec3 u_targetcolor;
 
 vec4 frag(vec2 pos, vec2 uv, vec4 color, sampler2D tex) {
-    vec4 o_color = def_frag();
-    if(o_color.r > .01 && o_color.g < .01 && o_color.b < .01)
+    vec4 o_color = texture2D(tex, uv);
+    if(distance(o_color.rgb, vec3(1., 0., 0.)) < 0.05)
         return vec4(u_targetcolor / 255. * o_color.a, o_color.a);
     return o_color;
 }
