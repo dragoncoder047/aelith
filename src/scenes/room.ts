c8e06f552073c37cc79fa4e70f2b1af38f96e598
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2025-09-24 20:27:15 -0400

    do run scripts of which there are none right now

--- src/scenes/room.ts
+++ src/scenes/room.ts
@@ -1,7 +1,12 @@
 import * as RoomManager from "../room/RoomManager";
 import * as BlueScreen from "../BlueScreen";
+import * as ScriptHandler from "../script/ScriptHandler";
+import { K } from "../context";
 
 export function roomScene(whichRoom: string) {
     BlueScreen.install();
     RoomManager.enterRoom(whichRoom);
+    K.onUpdate(() => {
+        ScriptHandler.advanceAsFarAsPossible();
+    });
 }
