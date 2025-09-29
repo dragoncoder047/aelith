bd37173e9dfaab7fb3803a1b70bd147cecd42fd1
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2025-09-29 09:03:29 -0400

    bump kaplay

--- src/entity/buildSkeleton.ts
+++ src/entity/buildSkeleton.ts
@@ -91,8 +91,6 @@ export function buildSkeleton(e: Entity, rootObj: GameObj<EntityComponents>): Bo
     const model = getEntityPrototypeStrict(e.kind).model;
     const constraintEntries: { c: EntityBoneConstraintOptData, t: string }[] = [];
     const ikEntries: { s: string, t: string, d: number }[] = [];
-    // temp fix for kaplayjs/kaplay#899
-    K.constraint.scale({} as any, {});
     const buildBone = (parent: GameObj, bonesList: EntityModelBoneData[]) => {
         for (var bone of bonesList) {
             const obj = parent.add([
